91dcc2095f0a2d45fbe4ca32020c2b1b70f14932
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-03-15 11:41:22 +0300

    add code from deleted app file

--- express-server/index.js
+++ express-server/index.js
@@ -1,13 +1,26 @@
-const mongoose = require('mongoose')
-const app = require('./app.js');
-const config = require('./config/main.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const PORT = config.port || 5555;
+const config = require('./config/main.js');
+const apiRoutes = require('./routes/index.js');
+
+const PORT = config.port;
+
+const app = express();
+
+app.use(logger('dev'));
+
+// Handling POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', apiRoutes);
 
 mongoose.connect(config.database);
 
 app.listen(PORT, () => {
 	console.log(`listening on localhost:${PORT}`);
 });
-
-
