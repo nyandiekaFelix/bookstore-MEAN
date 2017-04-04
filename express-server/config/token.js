7d5e0eac989d930524446f0e286b866cdf8efd36
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-04 10:52:15 +0300

    initial commit

---
+++ express-server/config/token.js
@@ -0,0 +1,40 @@
+const config = require('./main.js');
+const jwt = require('jsonwebtoken');
+
+
+module.exports = {
+    generateJWT: (user) => {
+        return jwt.sign(user, config.secret, {
+            expiresIn: 60 * 60 * 24
+        });
+    },
+
+    ensureAuthenticated: (req, res, next) => {
+        if (!req.header('Authorization')) {
+            return res.status(401).send({
+                message: 'The request made does not have an authorization header'
+            });
+        }
+
+        const id_token = req.header('Authorization').split(' ')[1];
+        let payload = null;
+
+        try{
+            payload = jwt.verify(id_token, config.secret);
+        }
+        catch (err) {
+            return res.status(401).send({
+                message: err.message
+            });
+        }
+
+        if (payload.expiresIn <= Date.now()) {
+            return res.status(401).json({
+                message: 'Token has expired'
+            });
+        }
+
+        /* Analyse, Combine, be awesome*/
+        req.user = pa
+    }
+};
