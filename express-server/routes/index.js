9c486605af1198d3c1f6ec5a563a79a6a46bb905
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-06 16:57:48 +0300

    add books routes

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./user.routes.js');
+const userRoutes = require('./user.routes');
+const booksRoutes = require('./books.routes')
 
 router.use('/user', userRoutes);
+router.use('/books', booksRoutes);
 
 module.exports = router;
