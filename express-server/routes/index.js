d35be80d09cacaa66b6baca7b579ab54f07f457f
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-11 11:38:26 +0300

    rename route

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./user.routes');
 const booksRoutes = require('./books.routes')
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/books', booksRoutes);
 
 module.exports = router;
