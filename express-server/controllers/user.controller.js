b47bb45ee190c4329eef933d2a99c890f252d311
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-17 14:59:06 +0300

    move login and signup methods to separate controller file

--- express-server/controllers/user.controller.js
+++ express-server/controllers/user.controller.js
@@ -1,68 +1,13 @@
-const gravatar = require('gravatar');
-
-const config = require('../config/main.js');
-const token = require('../config/token.js');
 const User = require('../models/user.model.js');
 
 module.exports = {
-    signup: (req, res) => {
-        User.findOne({ email: req.body.email }, (err, existingUser) => {
-            if (existingUser) {
-                return res.status(400).json({
-                    message: 'Sorry, that email is already registered to another account'
-                });
-            }
-
-            const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
-
-            const user = new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: avatarUrl
-            });
-
-            user.save((err) => {
-                if (err) {
-                    res.status(500).json({
-                        message: err.message
-                    });
-                }
-                res.status(201).send({
-                    id_token: token.generateJWT(user)
-                });
-            });
-        });
-    },
-
-    login: (req, res) => {
-        User.findOne({ email: req.body.email }, (err, user) => {
-            if (!user) {
-                return res.status(401).json({
-                    message: 'Wrong email address'
-                });
-            }
-
-            user.comparePassword(req.body.password, (err, ismatch) => {
-                if (!ismatch) {
-                    return res.status(401).json({
-                        message: 'Wrong password'
-                    });
-                }
-
-                res.status(201).send({
-                    id_token: token.generateJWT(user)
-                });
-            });
-        });
-    },
 
     getAllUsers: (req, res) => {
         User.find((err, users) => {
             if (err) {
                 res.json(err);
             }
-            res.send(users);
+            res.status(201).json(users);
         });
     },
 
@@ -73,7 +18,7 @@ module.exports = {
                     message: "Could not get user's profile details"
                 });
             }
-            res.send(user);
+            res.status(201).json(user);
         });
     },
 
