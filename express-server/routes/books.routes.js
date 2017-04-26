76835df8c92a8b128af9b9d31dc4bf3bc8759a6b
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-26 12:16:07 +0300

    remove unused route

--- express-server/routes/books.routes.js
+++ express-server/routes/books.routes.js
@@ -4,14 +4,12 @@ const booksCtrl = require('../controllers/books.controller');
 router.get('/', booksCtrl.getAllBooks);
 
 router.post('/upload', booksCtrl.create);
-router.get('/:userId', booksCtrl.getByUser);
 router.get('/:category', booksCtrl.getCategory);
 
 router.route('/:bookId')
     .get(booksCtrl.getOneBook)
     .put(booksCtrl.updateBook)
     .delete(booksCtrl.deleteBook);
-
-
+    
 module.exports = router;
 
