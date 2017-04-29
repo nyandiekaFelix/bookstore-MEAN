9f64f7d6057864119ae587421a438f87d2270ca4
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-29 11:51:24 +0300

    add error handling

--- express-server/controllers/books.controller.js
+++ express-server/controllers/books.controller.js
@@ -43,6 +43,9 @@ module.exports = {
             .populate()
             .exec()
             .then(book => {
+                if (!book) {
+                    return res.status(404).json({ message: 'Book not found' });
+                }
                 return res.status(200).json(book);
             })
             .catch(err => res.status(500).json(err));
