cc0a9f9022a0bd5296c3a266c3aef6d24ad56348
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-04-29 11:49:47 +0300

    exclude password field from response

--- express-server/controllers/auth.controller.js
+++ express-server/controllers/auth.controller.js
@@ -1,59 +1,68 @@
 const gravatar = require('gravatar');
 
-const config = require('../config/main.js');
-const token = require('../config/token.js');
-const User = require('../models/user.model.js');
+const config = require('../config/main');
+const token = require('../config/token');
+const User = require('../models/user.model');
 
 module.exports = {
     signup: (req, res) => {
-        User.findOne({ email: req.body.email }, (err, existingUser) => {
-            if (existingUser) {
-                return res.status(400).json({
-                    message: 'Sorry, that email is already registered to another account'
-                });
-            }
-
-            const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
-
-            const user = new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: avatarUrl
-            });
-
-            user.save((err) => {
-                if (err) {
-                    res.status(500).json({
-                        message: err.message
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(existingUser => {
+                if (existingUser) {
+                    return res.status(400).json({
+                        message: 'Sorry, that email is already registered to another account'
                     });
                 }
-                res.status(201).send({
-                    id_token: token.generateJWT(user)
+
+                const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
+
+                const user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: avatarUrl
                 });
-            });
-        });
+
+                user.save((err) => {
+                    if (err) {
+                        res.status(500).send(err);
+                    }
+                    
+                    user.password = undefined;
+                    res.status(201).send({
+                        id_token: token.generateJWT(user),
+                        user: user
+                    });
+                });
+            })
+            .catch(err => res.status(500).json(err));
     },
 
     login: (req, res) => {
-        User.findOne({ email: req.body.email }, (err, user) => {
-            if (!user) {
-                return res.status(401).json({
-                    message: 'Wrong email address'
-                });
-            }
-
-            user.comparePassword(req.body.password, (err, ismatch) => {
-                if (!ismatch) {
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(user => {
+                if (!user) {
                     return res.status(401).json({
-                        message: 'Wrong password'
+                        message: 'Wrong email address'
                     });
                 }
 
-                res.status(201).json({
-                    id_token: token.generateJWT(user)
+                user.comparePassword(req.body.password, (err, ismatch) => {
+                    if (!ismatch) {
+                        return res.status(401).json({
+                            message: 'Wrong password'
+                        });
+                    }
+
+                    user.password = undefined;
+                    res.status(201).json({
+                        id_token: token.generateJWT(user),
+                        user: user
+                    });
                 });
-            });
-        });
+            })
+            .catch(err => res.status(500).json(err));
     }
 };
