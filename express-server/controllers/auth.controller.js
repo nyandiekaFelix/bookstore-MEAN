617899eaf3987f620cff9873674d90df2062a18a
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-18 11:46:52 +0300

    add helper method to generate JWTs

--- express-server/controllers/auth.controller.js
+++ express-server/controllers/auth.controller.js
@@ -1,11 +1,19 @@
+const jwt =  require('jsonwebtoken');
 const gravatar = require('gravatar');
 
 const config = require('../config/main');
-const token = require('../config/token');
 const User = require('../models/user.model');
 
+const helpers = require('../helpers');
+
+function generateJWT(user) {
+    return jwt.sign(user, config.TOKEN_SECRET, {
+        expiresIn: 10080 // a week in seconds
+    });
+}
+
 module.exports = {
-    signup: (req, res) => {
+     registerUser: (req, res) => {
         User.findOne({ email: req.body.email })
             .exec()
             .then(existingUser => {
@@ -14,32 +22,36 @@ module.exports = {
                         message: 'Sorry, that email is already registered to another account'
                     });
                 }
-
+                
                 const avatarUrl = gravatar.url(req.body.email, { s: '200', r: 'x', d: 'retro' }, true);
 
                 const user = new User({
-                    username: req.body.username,
+                    profile: {
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName
+                    },
                     email: req.body.email,
                     password: req.body.password,
                     avatar: avatarUrl
                 });
 
+                
                 user.save((err) => {
                     if (err) {
                         res.status(500).send(err);
                     }
                     
-                    user.password = undefined;
+                    const detailsToReturn = helpers.setUserInfo(user);
                     res.status(201).send({
-                        id_token: token.generateJWT(user),
-                        user: user
+                        id_token: `JWT ${generateJWT(detailsToReturn)}`,
+                        user: detailsToReturn
                     });
                 });
             })
             .catch(err => res.status(500).json(err));
     },
 
-    login: (req, res) => {
+    loginUser: (req, res) => {
         User.findOne({ email: req.body.email })
             .exec()
             .then(user => {
@@ -50,19 +62,23 @@ module.exports = {
                 }
 
                 user.comparePassword(req.body.password, (err, ismatch) => {
+                    if (err) {
+                        return res.status(401).send(err);
+                    }
+
                     if (!ismatch) {
                         return res.status(401).json({
                             message: 'Wrong password'
                         });
                     }
 
-                    user.password = undefined;
-                    res.status(201).json({
-                        id_token: token.generateJWT(user),
-                        user: user
+                    const detailsToReturn = helpers.setUserInfo(user);
+                    res.status(201).send({
+                        id_token: `JWT ${generateJWT(detailsToReturn)}`,
+                        user: detailsToReturn
                     });
                 });
             })
             .catch(err => res.status(500).json(err));
     }
-};
+}
