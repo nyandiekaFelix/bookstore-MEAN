56253c74329630ac937842e607fc4d6f7d0b120a
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-05-18 11:43:21 +0300

    rename file

---
+++ express-server/config/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const config = require('./main');
+const User = require('../models/user.model');
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: config.TOKEN_SECRET
+};
+
+const jwtAuth = new JwtStrategy(jwtOptions, (payload, next) => {
+    User.findOne({ id: payload.id })
+        .exec()
+        .then(user => {
+            if (!user) {
+                return next(null, false, {
+                    error: 'User not found'
+                });
+            }
+
+            return next(null, user);
+        })
+        .catch(err => {
+            return next(err, false);
+        });
+});
+
+passport.use(jwtAuth);
