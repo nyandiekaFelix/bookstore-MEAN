b7d69666bf60b4f8e7c536153ee6e8749361c3da
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-06-08 11:33:52 +0300

    remove deleted components from declarations

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,21 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BooksComponent } from './books/books.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    BooksComponent,
-    ProfileComponent,
-    LoginComponent,
-    SignupComponent,
-    CheckoutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
