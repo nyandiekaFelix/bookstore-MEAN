6958987f04b523034b747504771deae3221b1759
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-06-21 16:53:59 +0300

    [refactor]: add 'get' and 'post' to base route

--- express-server/routes/books.routes.js
+++ express-server/routes/books.routes.js
@@ -6,9 +6,9 @@ const passportService = require('../config/passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 
-router.get('/', booksCtrl.getAllBooks);
-
-router.post('/upload', requireAuth, booksCtrl.create);
+router.route('/')
+    .get(booksCtrl.getAllBooks)
+    .post(requireAuth, booksCtrl.create);
 
 router.get('/category/:category', booksCtrl.getCategory);
 
